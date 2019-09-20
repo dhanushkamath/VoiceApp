d8548ff74966d68773780f6a9f64a26aaa55878c
dhanushkamath <dhanush2897@gmail.com>
2019-09-20 17:36:34 +0530

    Added limiter for locator results

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -13,8 +13,20 @@ export class CheckinComponent implements OnInit {
 
   ngOnInit() {
     this.checkinService.getNearbyBanks(51.511302, -0.124744, 1).subscribe(data =>{
-        this.banks = data['data'][0]['Brand'][0]['Branch'];
-        console.log(this.banks)
+        var results = data['data'][0]['Brand'][0]['Branch']
+
+        if (results.length !== 1) {
+          if (results.length > 6) {
+            results = results.slice(0, 6);
+          }
+
+          else if (results.length %2 !== 0) {
+            results = results.slice(0, results.length - 1);
+          }
+        }
+
+        this.banks = results;
+        console.log(this.banks);
     });
   }
 
